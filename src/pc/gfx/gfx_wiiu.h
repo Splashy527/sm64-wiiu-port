2088dfc7febeda3c5cc1ddd327215690747080f4
Nadia Holmquist Pedersen <nadia@nhp.sh>
2020-08-13 23:38:52 +0200

    Wii U: Drop SDL for graphics, use ProcUI manually.

---
+++ src/pc/gfx/gfx_wiiu.h
@@ -0,0 +1,12 @@
+#ifndef GFX_WIIU_H
+#define GFX_WIIU_H
+
+#ifdef TARGET_WII_U
+
+#include "gfx_window_manager_api.h"
+
+extern struct GfxWindowManagerAPI gfx_wiiu;
+extern bool gfx_wiiu_running;
+
+#endif
+#endif
