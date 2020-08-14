8e47474fe0acbb3715c9c38e4dec477bf2680b97
Nadia Holmquist Pedersen <nadia@nhp.sh>
2020-08-14 14:26:31 +0200

    Clean includes and get rid of unused callbacks

--- src/pc/gfx/gfx_wiiu.c
+++ src/pc/gfx/gfx_wiiu.c
@@ -1,6 +1,5 @@
 #ifdef TARGET_WII_U
 
-#include <coreinit/exit.h>
 #include <coreinit/foreground.h>
 #include <whb/gfx.h>
 #include <whb/log.h>
@@ -8,12 +7,8 @@
 #include <gx2/event.h>
 #include <gx2/swap.h>
 #include <proc_ui/procui.h>
-#include <whb/gfx.h>
-#include <whb/log.h>
 #include <whb/log_cafe.h>
 #include <whb/log_udp.h>
-#include <whb/crash.h>
-#include <stdlib.h>
 #include <macros.h>
 #include <sndcore2/core.h>
 
@@ -25,11 +20,6 @@
 static unsigned int window_width;
 static unsigned int window_height;
 static bool is_running;
-static bool fullscreen_state = false;
-static void (*on_fullscreen_changed_callback)(bool is_now_fullscreen);
-static bool (*on_key_down_callback)(int scancode);
-static bool (*on_key_up_callback)(int scancode);
-static void (*on_all_keys_up_callback)(void);
 
 void save_callback(void) {
     OSSavesDone_ReadyToRelease();
@@ -70,10 +60,6 @@ static void gfx_wiiu_get_dimensions(uint32_t *width, uint32_t *height) {
 }
 
 static void set_fullscreen(bool on, bool call_callback) {
-    // We don't support not running in full screen
-    if (on_fullscreen_changed_callback != NULL && call_callback) {
-        on_fullscreen_changed_callback(on);
-    }
 }
 
 static void gfx_wiiu_init(UNUSED const char *game_name, UNUSED bool start_in_fullscreen) {
@@ -89,7 +75,6 @@ static void gfx_wiiu_init(UNUSED const char *game_name, UNUSED bool start_in_ful
 }
 
 static void gfx_wiiu_set_fullscreen_changed_callback(void (*on_fullscreen_changed)(bool is_now_fullscreen)) {
-    on_fullscreen_changed_callback = on_fullscreen_changed;
 }
 
 static void gfx_wiiu_set_fullscreen(bool enable) {
@@ -97,9 +82,6 @@ static void gfx_wiiu_set_fullscreen(bool enable) {
 }
 
 static void gfx_wiiu_set_keyboard_callbacks(bool (*on_key_down)(int scancode), bool (*on_key_up)(int scancode), void (*on_all_keys_up)(void)) {
-    on_key_down_callback = on_key_down;
-    on_key_up_callback = on_key_up;
-    on_all_keys_up_callback = on_all_keys_up;
 }
 
 static void gfx_wiiu_main_loop(void (*run_one_game_iter)(void)) {
@@ -113,7 +95,6 @@ static void gfx_wiiu_onkeyup(UNUSED int scancode) {
 }
 
 static void gfx_wiiu_handle_events(void) {
-
 }
 
 bool gfx_wiiu_is_running(void) {
@@ -125,7 +106,6 @@ bool gfx_wiiu_is_running(void) {
 
     switch (status) {
         case PROCUI_STATUS_EXITING:
-            WHBLogPrint("Going to exit");
             is_running = false;
             return false;
         case PROCUI_STATUS_RELEASE_FOREGROUND:
